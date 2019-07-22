e106defb65240072e9be076b114379a2495018f4
Kayla Reopelle <kreopelle@gmail.com>
2019-07-22 00:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[string.length - 1]);
+  if (string.length > 1) {
+    let substring = string.substring(0, string.length - 1);
+    printString(substring);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if (string.length > 0) {
+    return reverseString(string.substring(1)) + string[0]
+  } else {
+    return "";
+  }
+}
